e8cc18db907c71f34fb676e2ba30639ed53185e1
John R. Leeman <john@leemangeophysical.com>
2020-07-10 13:07:56 -0500

    Add export of read_binary

--- pylook/io/lookfiles.py
+++ pylook/io/lookfiles.py
@@ -2,7 +2,7 @@
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 
-"""Contains utilities to work with "look" style data files."""
+"""Contains utilities to work with "look" style data files and associated "r" files."""
 
 import struct
 import warnings
